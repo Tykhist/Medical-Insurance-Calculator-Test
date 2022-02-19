59bc12b570c9388822c4e3f2db977b7339cbefd6
William Smith <75560670+Tykhist@users.noreply.github.com>
2022-02-19 11:10:12 -0800

    Create Test2.py
    Test for a 35-year-old male who smokes, has 2 children, and a BMI of 27.3

---
+++ Test2.py
@@ -0,0 +1,56 @@
+# We will calculate the cost of insurance for a 35-year-old male who smokes, has 2 children, and a BMI of 27.3
+age = 35
+smoker = 1   # 0 for a non-smoker, 1 for a smoker
+sex = 1   # 0 for female, 1 for male (assigned gender at birth)
+bmi = 27.3
+num_of_children = 2
+
+# insurance estimate formula below
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+print('This person\'s insurance cost is', insurance_cost, 'dollars.\n')
+
+# Age Factor: to see how much of a factor age plays in the formula, we will increase it and then find the difference between the new and old insurance costs
+age += 4
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print('The change in cost of insurance after increasing the age by 4 years is', change_in_insurance_cost, 'dollars.\n')
+# BMI Factor: same method as age factor, except we will revert age to its original value
+age -= 4
+bmi += 3.1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print('The change estimated insurance cost after increasing BMI by 3.1 is', change_in_insurance_cost, 'dollars.\n')
+# Male vs. Female Factor: same method as bmi factor, except we will revert bmi to its original value
+bmi -= 3.1
+sex = 0
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print('The change in estimated cost for being female instead of male is', change_in_insurance_cost, 'dollars.\n')
+# Smoker Factor: same method as Male vs. Female factor, except we will revert sex to its original value
+sex = 1
+smoker = 0
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print('The change in estimated cost for being a nonsmoker is', change_in_insurance_cost, 'dollars.\n')
+# And finally, Number of Children Factor: same method as smoker factor, except we will revert smoker to its original value
+smoker = 1
+num_of_children += 2
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print('The change in estimated cost for having an additional 2 children is', change_in_insurance_cost, 'dollars.')
